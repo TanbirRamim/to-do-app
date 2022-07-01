191a491353bd7052c4388c2822cf4db1d2687c34
TanbirRamim <Tanbirramim420@gmail.com>
2022-07-01 16:31:31 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-center font-bold ">
+        It's the beginning of my to-do app
+      </h1>
+    </div>
+  );
+}
+
+export default App;
